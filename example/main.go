a59a7c6184ae9a5002f650a45f79ff9734cfd850
Ricky Grassmuck <rigrassm@gmail.com>
2022-08-16 01:42:00 +0000

    Update package directory name

--- example/main.go
+++ example/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	lichess "github.com/RickyGrassmuck/lichess-go/lichess"
+)
+
+func main() {
+	c, err := lichess.NewClient("https://lichess.org")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	c.ApiUser(context.Background(), "joyrida")
+}
